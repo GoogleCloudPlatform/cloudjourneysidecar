dc3bc4d4910ba1937b72389be73d262883309b2f
Terrence Ryan <tpryan@google.com>
2019-04-26 15:48:48 -0700

    syncing from source

--- publish.sh
+++ publish.sh
@@ -4,15 +4,3 @@ $(cp -r $PWD/* $CLOUD_JOURNEY_SIDECAR_PATH)
 echo git adding to $CLOUD_JOURNEY_SIDECAR_PATH
 (cd $CLOUD_JOURNEY_SIDECAR_PATH && git add * )
 (cd $CLOUD_JOURNEY_SIDECAR_PATH && git commit -m "syncing from source" )
-
-# git add * 
-# git commit -m "syncing from source"
-# deploy: 
-# 	cp -r $(BASEDIR)/* $(GITFOLDER)
-# 	cd $(CLOUD_JOURNEY_TUTORIALS_PATH) && \
-# 	git add * && \
-# 	git commit -m "syncing from source" 
-	
-
-# publish:
-# 	cd $(CLOUD_JOURNEY_TUTORIALS_PATH) && git push
