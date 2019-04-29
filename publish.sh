1a64363f890ee9da93116daffde4ac8b138b3854
Terrence Ryan <tpryan@google.com>
2019-04-29 14:52:00 -0700

    removing and deleting publish.sh

--- publish.sh
+++
@@ -1,6 +0,0 @@
-
-echo cp to $CLOUD_JOURNEY_SIDECAR_PATH
-$(cp -r $PWD/* $CLOUD_JOURNEY_SIDECAR_PATH)
-echo git adding to $CLOUD_JOURNEY_SIDECAR_PATH
-(cd $CLOUD_JOURNEY_SIDECAR_PATH && git add * )
-(cd $CLOUD_JOURNEY_SIDECAR_PATH && git commit -m "syncing from source" )
