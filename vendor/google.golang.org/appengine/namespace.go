f9a50bc6579faf00de8b1bc66a101a59fc39d777
Terrence Ryan <tpryan@google.com>
2019-04-29 14:39:22 -0700

    syncing from source

--- vendor/google.golang.org/appengine/namespace.go
+++ vendor/google.golang.org/appengine/namespace.go
@@ -1,4 +1,4 @@
-// Copyright 2012 Google Inc. All rights reserved.
+// Copyright 2012 Google LLC All rights reserved.
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
